f01b3cc38457bf98b0d1b2425f9c40006b55a118
umharu <maxiroldancapponi@gmail.com>
2024-12-08 23:37:26 -0300

    Clean some things

--- nft/views.py
+++ nft/views.py
@@ -9,27 +9,6 @@ def index(request):
     return render(request, 'index.html')
 
 
-
-def create_drug(request):
-    # Página de formulario para agregar datos al contrato
-    if request.method == 'POST':
-        # Implementar la lógica para agregar datos al contrato
-        pass
-    return render(request, 'create_drug.html')
-
-
-def change_owner(request):
-    # Página de formulario para cambiar el propietario en el contrato
-    if request.method == 'POST':
-        # Implementar la lógica para cambiar el propietario
-        pass
-    return render(request, 'change_owner.html')
-
-
-from django.shortcuts import render
-from web3 import Web3
-from django.conf import settings
-
 def interact(request):
     w3 = Web3(Web3.HTTPProvider(settings.WEB3_PROVIDER))
     
